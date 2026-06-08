1366df5769d5ce56317d4ff00308e6bd1462f15d
RaihanRafi12 <raihanrafi9876@gmail.com>
2026-06-09 02:16:36 +0600

    Update urls.py

--- StudyBot-main-database-setup/backend/api/urls.py
+++ StudyBot-main-database-setup/backend/api/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from . import views
+from ninja import NinjaAPI
+from api.auth_endpoints import router as auth_router
+from api.resource_endpoints import router as resource_router
+from api.admin_endpoints import router as admin_router
+from api.user_endpoints import router as user_router
+
+api = NinjaAPI()
+
+api.add_router("/auth/", auth_router)
+api.add_router("/resources/", resource_router)
+api.add_router("/admin/", admin_router)
+api.add_router("/users/", user_router)
 
 urlpatterns = [
-    path('', views.health_check, name='health'),
+    path("", api.urls),
 ]
